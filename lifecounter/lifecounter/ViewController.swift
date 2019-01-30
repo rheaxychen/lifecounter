5bcee1b96e5366c64f43fb787cac2f101bed191a
Rhea X Chen <xinyuc2@uw.edu>
2019-01-29 21:05:16 -0800

    Finish & Add screenshots

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -23,37 +23,34 @@ class ViewController: UIViewController {
 	@IBOutlet weak var message: UILabel!
 	
 	@IBAction func buttons(_ sender: UIButton) {
-		if p1counter == 0 { message.text = "Player 1 LOSES!" }
-		if p2counter == 0 { message.text = "Player 2 LOSES!" }
-		if p3counter == 0 { message.text = "Player 3 LOSES!" }
-		if p4counter == 0 { message.text = "Player 4 LOSES!" }
 		
-		if sender.tag < 10 {
+		if sender.tag < 10 && p1counter > 0 {
 			if sender.tag == 1 { p1counter -= 1 }
 			if sender.tag == 2 { p1counter -= 5 }
 			if sender.tag == 3 { p1counter += 5 }
 			if sender.tag == 4 { p1counter += 1 }
+			if p1counter <= 0 { message.text = "Player 1 LOSES!" }
 			p1score.text = String(p1counter)
-		}
-		if sender.tag < 20 && sender.tag > 10{
+		} else if sender.tag < 20 && p2counter > 0 {
 			if sender.tag == 11 { p2counter -= 1 }
 			if sender.tag == 12 { p2counter -= 5 }
 			if sender.tag == 13 { p2counter += 5 }
 			if sender.tag == 14 { p2counter += 1 }
+			if p2counter <= 0 { message.text = "Player 2 LOSES!" }
 			p2score.text = String(p2counter)
-		}
-		if sender.tag < 30 && sender.tag > 20{
+		} else if sender.tag < 30 && p3counter > 0 {
 			if sender.tag == 21 { p3counter -= 1 }
 			if sender.tag == 22 { p3counter -= 5 }
 			if sender.tag == 23 { p3counter += 5 }
 			if sender.tag == 24 { p3counter += 1 }
+			if p3counter <= 0 { message.text = "Player 3 LOSES!" }
 			p3score.text = String(p3counter)
-		}
-		if sender.tag > 30 {
+		} else if sender.tag > 30 && p4counter > 0  {
 			if sender.tag == 31 { p4counter -= 1 }
 			if sender.tag == 32 { p4counter -= 5 }
 			if sender.tag == 33 { p4counter += 5 }
 			if sender.tag == 34 { p4counter += 1 }
+			if p4counter <= 0 { message.text = "Player 4 LOSES!" }
 			p4score.text = String(p4counter)
 		}
 	}
