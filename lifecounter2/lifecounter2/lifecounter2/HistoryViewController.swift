2d90f12ec0c4b0edf512cdacb7b71af2f1cc46d9
Rhea X Chen <xinyuc2@uw.edu>
2019-02-03 18:04:26 -0800

    Add Lifecounter2 UI

---
+++ lifecounter2/lifecounter2/lifecounter2/HistoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryViewController.swift
+//  lifecounter2
+//
+//  Created by Rhea on 2/3/19.
+//  Copyright © 2019 Rhea. All rights reserved.
+//
+
+import Foundation
---
+++ lifecounter2/lifecounter2/lifecounter2/PlayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter2
+//
+//  Created by Rhea on 2/3/19.
+//  Copyright © 2019 Rhea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
